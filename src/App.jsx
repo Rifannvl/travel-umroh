47887892853bf3ac11c8e4e5b56184ec9ebeb245
Rifannvl <riffanafendi@gmail.com>
2024-11-25 23:42:06 +0700

    menambah style untuk komponen yang di install

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Importing PrimeReact's core CSS and theme
+import "primereact/resources/themes/lara-light-indigo/theme.css"; // Choose the theme you prefer
+import "primereact/resources/primereact.min.css"; // PrimeReact core styles
+
 import Home from "./pages/Home";
 
 export default function App() {
-  return;
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
