01522ce1a9df42d2561bebf8663f9c3c20b063cb
Rifannvl <riffanafendi@gmail.com>
2024-11-25 23:42:20 +0700

    menambahkan komponen ke home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,10 @@
+import React from "react";
+import CarouselDemo from "../components/carousel";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Home</h1>;
+  return (
+    <div>
+      <CarouselDemo />
+    </div>
+  );
 }
